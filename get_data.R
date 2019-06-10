1f050d67aaeeed8ca605f87a285faafd9a141b20
JonathanB13 <hezibuba@mail.tau.ac.il>
2019-06-10 19:31:09 +0300

    Added library call

--- get_data.R
+++ get_data.R
@@ -1,6 +1,7 @@
 # this file simply gets the data. If you want to get the raw data or read more
 # about the functions used, read the comments on "googlesheets.R"
 
+library("tidyverse")
 
 source("googlesheets.R")
 
@@ -8,5 +9,7 @@ bioblitzdata <- create_full_data() %>%
   clean_full_data() %>% 
   add_species_data()
 
-#this saves the data as an R object if you wish:
-saveRDS(object = bioblitzdata,file = "bioblitzdata",version = 3)
+# this saves the data as an R object if you wish:
+# saveRDS(object = bioblitzdata,file = "bioblitzdata",version = 3)
+# You can later load it like so:
+# bioblitzdata <- readRDS('~/bioblitzdata.RDS')
